280f9385814a2c490397510a2089841a404c14a4
Lewis Benson <tm.lewis.benson@gmail.com>
2022-09-29 15:34:10 -0400

    add objects to options, stop page from reloading, update card preview

--- js/catalog.js
+++ js/catalog.js
@@ -12,7 +12,9 @@ function populateForm() {
   //TODO: Add an <option> tag inside the form's select for each product
   const selectElement = document.getElementById('items');
   for (let i in Product.allProducts) {
-
+    const optionElement = document.createElement('option'); 
+    optionElement.innerText = Product.allProducts[i].name; 
+    selectElement.appendChild(optionElement); 
   }
 
 }
@@ -21,7 +23,7 @@ function populateForm() {
 // object, save the whole thing back to local storage and update the screen
 // so that it shows the # of items in the cart and a quick preview of the cart itself.
 function handleSubmit(event) {
-
+  event.preventDefault(); 
   // TODO: Prevent the page from reloading
 
   // Do all the things ...
@@ -37,10 +39,16 @@ function addSelectedItemToCart() {
   // TODO: suss out the item picked from the select list
   // TODO: get the quantity
   // TODO: using those, add one item to the Cart
+  cart.addItem(document.getElementById('items').value, document.getElementById('quantity').value); //!
+  
 }
 
 // TODO: Update the cart count in the header nav with the number of items in the Cart
-function updateCounter() { }
+function updateCounter() {
+  const cartContents = document.getElementById('cartContents');
+  const newItemtext = document.createElement('li');
+  newItemtext.innerText = `${document.getElementById('items').value} - ${document.getElementById('quantity').value}x`
+ }
 
 // TODO: As you add items into the cart, show them (item & quantity) in the cart preview div
 function updateCartPreview() {
